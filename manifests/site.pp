eef5e13f60e1e84df321184600d3f0b3897ee979
Andrew Fomych <afomich610@gmail.com>
2022-06-15 12:17:20 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node default{
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is readme file',
-    owner   => 'root',
-  }
-  
-  file {'/root/README':
-    owner   => 'root',
-  }
+
 }

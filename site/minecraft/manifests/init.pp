b84f7f9364d2246a770243508c3ecd1a574a41c6
Andrew Fomych <afomich610@gmail.com>
2022-06-16 14:03:37 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -8,8 +8,12 @@ class minecraft{
     source => 'https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar'
   }
   
-  package{'java':
-    ensure => present,
+  java::adoptium { 'jdk17':
+    ensure  => 'present',
+    version_major => '17',
+    version_minor => '0',
+    version_patch => '3',
+    version_build => '1',
   }
   
   file{'/opt/minecraft/eula.txt':

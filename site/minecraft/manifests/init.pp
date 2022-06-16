3eb07b8a12a29c11415a48bae1e0654a4094b147
Andrew Fomych <afomich610@gmail.com>
2022-06-16 17:03:28 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -48,7 +48,9 @@ class minecraft(
   /*** File for autostart server ***/
   file{'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp('minecraft/minecraft.service', {
+      install_dir => $install_dir
+    }),
   }
   /*****************************/
   

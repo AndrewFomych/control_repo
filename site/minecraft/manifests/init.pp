4c208c448bc794500ebdc0cde61196b14cd4cd22
Andrew Fomych <afomich610@gmail.com>
2022-06-16 15:39:58 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -13,9 +13,13 @@ class minecraft{
     ensure => present,
   }
   
-  file{'/opt/minecraft/server.jar':
+  /*file{'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar',
+  }*/
+  file {‘/opt/minecraft/server.jar’:
+    ensure => file,
+    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
   
   file{'/opt/minecraft/eula.txt':

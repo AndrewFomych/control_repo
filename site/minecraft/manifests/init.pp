1e0cf053c4ed73c4c3988a3b603c9a7effef8d2c
Andrew Fomych <afomich610@gmail.com>
2022-06-16 15:11:26 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -12,7 +12,7 @@ class minecraft{
   java::download { 'jdk17':
     ensure  => 'present',
     java_se => 'jdk',
-    url     => 'https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz',
+    url     => 'openjdk-17.0.2_linux-x64_bin.tar.gz',
   }
   
   file{'/opt/minecraft/eula.txt':

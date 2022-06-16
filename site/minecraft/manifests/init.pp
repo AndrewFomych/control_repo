a60329720fddeedbb0d41a40c0f40c556b429d45
Andrew Fomych <afomich610@gmail.com>
2022-06-16 14:04:57 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -8,6 +8,8 @@ class minecraft{
     source => 'https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar'
   }
   
+  include java
+  
   java::adoptium { 'jdk17':
     ensure  => 'present',
     version_major => '17',
